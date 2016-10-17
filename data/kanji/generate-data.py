614e5699164aaedb07a48484d12a5387f7c2bb05
Panupong Pasupat <ppasupat@stanford.edu>
2016-10-16 20:35:50 -0700

    Kanji generator

---
+++ data/kanji/generate-data.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys, os, shutil, re, argparse, json
+from codecs import open
+from itertools import izip
+from collections import defaultdict, Counter, OrderedDict
+
+from kanjitools import get_cats
+
+def create_root():
+    root = OrderedDict()
+    root['books'] = []
+    root['highlights'] = {}
+    return root
+
+def create_book(root, shortname, name):
+    book = OrderedDict()
+    book['shortname'] = shortname
+    book['name'] = name
+    book['chapters'] = []
+    root['books'].append(book)
+    return book
+
+def create_chapter(book, name, kanji):
+    chapter = OrderedDict()
+    chapter['name'] = name
+    chapter['kanji'] = kanji
+    book['chapters'].append(chapter)
+    return chapter
+
+################################
+# GRADE
+
+def grade_generator(args):
+    root = create_root()
+    cats = get_cats()
+    # Read onyomi groups
+    onyomi_groups = dict([x for x in cats.iteritems() if x[0].startswith('JO')])
+    # Read grades
+    for name, cat in [
+            ('E1', 'E1'), ('E2', 'E2'), ('E3', 'E3'),
+            ('E4', 'E4'), ('E5', 'E5'), ('E6', 'E6'),
+            ('J1', 'KK4'), ('J2', 'KK3'), ('J3', 'KK2.5'), ('J4', 'KK2')]:
+        book = create_book(root, name, name)
+        kanji_from_grade = set(cats[cat])
+        for c1, label in zip('_KSTNHMYRW', u'アカサタナハマヤラワ'):
+            kanji = []
+            for c2 in 'AIUEO':
+                key = 'JO' + c1 + c2
+                if key not in onyomi_groups:
+                    continue
+                kanji.append(''.join(x for x in onyomi_groups[key] if x in kanji_from_grade))
+            kanji = '|'.join(x for x in kanji if x)
+            if kanji:
+                create_chapter(book, label, kanji)
+    return root
+
+################################
+
+GENERATORS = {
+        'grade': grade_generator,
+
+        }
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('name', choices=GENERATORS)
+    parser.add_argument('-a', '--aux-file')
+    args = parser.parse_args()
+
+    root = GENERATORS[args.name](args)
+    print json.dumps(root, ensure_ascii=False, indent=2, separators=(',', ':'))
+
+if __name__ == '__main__':
+    main()
+
--- static/main.js
+++ static/main.js
@@ -1,25 +1,15 @@
 $(function () {
-  "use strict";
+  'use strict';
   var settings = loadSettings();
   console.log(settings);
 
-  $.get('data/kanji/grade.txt', function (data) {
-    console.log(data);
-  });
+  var kanjiFile = 'data/kanji/' + settings.k + '.json';
 
-  var vocab_xml = "data/jsl-vocablist-filtered.xml",
-      jlpt_txt = "data/jlpt3-5.html",
-      jlpt_2 = "data/jlpt2.html",
-      kago_json = "data/kago.json",
-      xiuwenyuan_json = "data/xiuwenyuan.json",
-      list_name = gup("list"),
-      kanji_xml = {
-        "old":    "data/kanjilist-old.xml",
-        "extra":  "data/kanjilist-extra.xml",
-        "neo":    "data/kanjilist-neo.xml",
-        "kklc":   "data/kanjilist-kklc.xml",
-        "rtk":    "data/kanjilist-rtk.xml",
-      }[list_name] || "data/kanjilist.xml",
+  var vocab_xml = 'data/jsl-vocablist-filtered.xml',
+      jlpt_txt = 'data/jlpt3-5.html',
+      jlpt_2 = 'data/jlpt2.html',
+      kago_json = 'data/kago.json',
+      xiuwenyuan_json = 'data/xiuwenyuan.json',
       parts_not_loaded = 6;
 
   // Load data
@@ -27,19 +17,19 @@ $(function () {
     parts_not_loaded--;
     if (parts_not_loaded === 0) {
       // Specify ?q=___ to query the character
-      var character = gup("q");
+      var character = gup('q');
       if (character) {
-        $("#txtChar").val(character);
-        $("#formChar").submit();
+        $('#txtChar').val(character);
+        $('#formChar').submit();
       }
     }
   };
 
   var words = [];
   $.get(vocab_xml, function (data) {
-    $(data).find("chapter").each(function (index, elt) {
-      var chapter_name = $(elt).attr("name");
-      $(elt).text().split("\n").forEach(function (word) {
+    $(data).find('chapter').each(function (index, elt) {
+      var chapter_name = $(elt).attr('name');
+      $(elt).text().split('\n').forEach(function (word) {
         if (word) {
           words.push([word, chapter_name]);
         }
@@ -50,127 +40,50 @@ $(function () {
 
   var jlptWords = [], jlpt2Words = [];
   $.get(jlpt_txt, function (data) {
-    data.split("\n").forEach(function (word) {
+    data.split('\n').forEach(function (word) {
       jlptWords.push(word);
     });
     increment_loaded();
   });
   $.get(jlpt_2, function (data) {
-    data.split("\n").forEach(function (word) {
+    data.split('\n').forEach(function (word) {
       jlpt2Words.push(word);
     });
     increment_loaded();
   });
 
   var kanjis = {};
-  $.get(kanji_xml, function (data) {
-    var extras = {}, h5 = {}, h6 = {}, h6x = {}, moved = {},
-        originals = {}, jouyou = {}, chrCnt = 0;
-    $(data).find("extra chapter").each(function (index, elt) {
-      var type = $(elt).attr("type"), name = $(elt).attr("name"),
-          target = undefined;
-      if (type === "kyouiku") {
-        target = extras;
-      } else if (type === "moved") {
-        target = moved;
-      } else if (type === "harvard") {
-        if (name === "5") {
-          target = h5;
-        } else if (name === "6") {
-          target = h6;
-        } else if (name === "6x") {
-          target = h6x;
-        }
-      } else if (type === 'jouyou') {
-        target = jouyou;
-      }
-      if (target) {
-        $(elt).text().split("").forEach(function (chr) {
-          target[chr] = true;
-        });
-      }
-    });
-    $(data).find("originals").text().split("").forEach(function (chr) {
-      originals[chr] = true;
-    });
-    $(data).find("book").each(function (index, elt) {
-      var bookNode = $(elt);
-      var bookCharElt = $("<div>").addClass("book-char")
-            .appendTo($("#bookChar")).hide();
-      var bookListElt = $("<div>")
-            .addClass("book-list pink-button unselectable")
-            .text(bookNode.attr("shortname"))
-            .attr("title", bookNode.attr("name"))
-            .appendTo($("#bookList"))
+  $.get(kanjiFile, function (data) {
+    data.books.forEach(function (book) {
+      var bookCharElt = $('<div class=book-char>')
+            .appendTo('#bookChar').hide();
+      var bookListElt = $('<div>')
+            .addClass('book-list pink-button unselectable')
+            .text(book.shortname).attr('title', book.name)
+            .appendTo('#bookList')
             .click(function () {
-              $(".book-list").removeClass("active");
-              $(".book-char").hide();
-              bookListElt.addClass("active");
+              $('.book-list').removeClass('active');
+              $('.book-char').hide();
+              bookListElt.addClass('active');
               bookCharElt.show();
             });
-      var addenda = {};
-      bookNode.find("addendum").text().split("").forEach(function (chr) {
-        if (addenda[chr] !== undefined) {
-          console.log("Warning! Repeated addendum character: " + chr);
-        }
-        addenda[chr] = true;
-      });
-      bookNode.find("chapter").each(function (index, elt) {
-        var chapterNode = $(elt);
-        var currentChapter = chapterNode.text().split("");
-        var toBeAdded = $("<p>");
-        toBeAdded.append($("<span>").addClass("title")
-                         .text(chapterNode.attr("name")));
-        currentChapter.forEach(function (chr) {
-          if (chr === "|") {
-            toBeAdded.append(" | ");
+      book.chapters.forEach(function (chapter) {
+        var chapterElt = $('<p>').appendTo(bookCharElt)
+          .append($('<span class=title>').text(chapter.name));
+        chapter.kanji.split('').forEach(function (chr) {
+          if (chr === '|') {
+            chapterElt.append(' | ');
           } else if (chr === '(' || chr === ')') {
-            toBeAdded.append(chr);
-          } else if (chr !== "" && chr !== " ") {
-            if (originals[chr]) {
-              chrCnt += 1;
-              if (chrCnt % 100 == 1 && chrCnt > 1) {
-                var s = String.fromCharCode(9311 + chrCnt/100);
-                toBeAdded.append(" " + s);
-              }
-            }
-            var charSpan = $("<span class='chr'>").text(chr);
-            toBeAdded.append(" ").append(charSpan);
+            chapterElt.append(chr);
+          } else if (chr !== ' ') {
+            var charSpan = $('<span class=chr>').text(chr);
+            chapterElt.append(' ').append(charSpan);
             if (kanjis[chr] !== undefined) {
-              console.log("Warning! Repeated character: " + chr);
+              console.log('Warning! Repeated character: ' + chr);
             }
             kanjis[chr] = [bookListElt, charSpan];
-            if (list_name === "extra" && !originals[chr]) {
-              if (h5[chr]) {
-                charSpan.addClass("h5");
-              } else if (h6[chr]) {
-                charSpan.addClass("h6");
-              } else if (h6x[chr]) {
-                charSpan.addClass("h6x");
-              } else if (extras[chr]) {
-                charSpan.addClass("extra");
-              } else {
-                charSpan.addClass("what");
-              }
-              if (addenda[chr]) {
-                charSpan.addClass("addendum");
-                delete addenda[chr];
-              }
-            } else if (list_name === 'neo') {
-              if (!jouyou[chr]) {
-                charSpan.addClass("extra");
-              }
-            }
-            if (moved[chr]) {
-              charSpan.addClass("moved");
-            }
           }
         });
-        if (countObjectKeys(addenda.length) !== 0) {
-          console.log("Warning! Remaining addendum characters:");
-          console.log(addenda);
-        }
-        bookCharElt.append(toBeAdded);
       });
     });
     increment_loaded();
@@ -178,27 +91,27 @@ $(function () {
 
   /////////
 
-  $("#bookChar").on("click", ".chr", function () {
-    $("#txtChar").val($(this).text());
-    $("#formChar").submit();
+  $('#bookChar').on('click', '.chr', function () {
+    $('#txtChar').val($(this).text());
+    $('#formChar').submit();
   });
 
   // Query functions
   var colorCode = function (word) {
-    var s = "60%";
-    var l = "85%";
+    var s = '60%';
+    var l = '85%';
     var h;
-    if (word <= "JSL 06 B") {
+    if (word <= 'JSL 06 B') {
       h = 0;
-    } else if (word <= "JSL 12 B") {
+    } else if (word <= 'JSL 12 B') {
       h = 60;
-    } else if (word <= "JSL 17 B") {
+    } else if (word <= 'JSL 17 B') {
       h = 120;
-    } else if (word <= "JSL 22 B") {
+    } else if (word <= 'JSL 22 B') {
       h = 180;
-    } else if (word <= "JSL 27 B") {
+    } else if (word <= 'JSL 27 B') {
       h = 240;
-    } else if (word <= "JSL 30 B") {
+    } else if (word <= 'JSL 30 B') {
       h = 300;
     } else {
       h = 360;
@@ -206,92 +119,92 @@ $(function () {
     return 'hsl(' + h + ',' + s + ',' + l + ')';
   };
 
-  var BLANK = "\u3000";
+  var BLANK = '\u3000';
 
-  var wwwjdicUrl = "http://www.edrdg.org/cgi-bin/wwwjdic/wwwjdic?1MUJ";
+  var wwwjdicUrl = 'http://www.edrdg.org/cgi-bin/wwwjdic/wwwjdic?1MUJ';
   var getAnchor = function (text) {
-    return $("<a>").text(text)
+    return $('<a>').text(text)
       .attr('href', wwwjdicUrl + text)
       .attr('target', 'extFrame');
   }
 
   var query = function () {
-    var c = firstChar($("#txtChar").val());
+    var c = firstChar($('#txtChar').val());
     // Clear the display
-    $("#wordList, #jlptList").empty();
-    if (!c || c === " ") {
-      $("#theChar").text(BLANK);
-      $("#charDesc").hide();
+    $('#wordList, #jlptList').empty();
+    if (!c || c === ' ') {
+      $('#theChar').text(BLANK);
+      $('#charDesc').hide();
       return;
     }
-    $("#theChar").text(c);
+    $('#theChar').text(c);
     var pattern = new RegExp(c);
     $.grep(words, function (tuple, index) {
       return pattern.test(tuple[0]);
     }).forEach(function (tuple) {
-      $("#wordList").append(getAnchor(tuple[0])
-                            .css("background-color", 
+      $('#wordList').append(getAnchor(tuple[0])
+                            .css('background-color', 
                                  colorCode(tuple[1])));
     });
     $.grep(jlptWords, function (word, index) {
       return pattern.test(word);
     }).forEach(function (word) {
-      $("#jlptList").append(getAnchor(word));
+      $('#jlptList').append(getAnchor(word));
     });
     $.grep(jlpt2Words, function (word, index) {
       return pattern.test(word);
     }).forEach(function (word) {
-      $("#jlptList").append(getAnchor(word).addClass('n2'));
+      $('#jlptList').append(getAnchor(word).addClass('n2'));
     });
-    $("#charDesc").show();
+    $('#charDesc').show();
     
     var toHighlight = kanjis[c];
-    $(".chr").removeClass("highlight");
+    $('.chr').removeClass('highlight');
     if (toHighlight) {
       toHighlight[0].click();
-      toHighlight[1].addClass("highlight");
+      toHighlight[1].addClass('highlight');
     } else {
-      $(".book-list").removeClass("active");
-      $(".book-char").hide();
+      $('.book-list').removeClass('active');
+      $('.book-char').hide();
     }
     goToSite();
   };
   
-  $("#formChar").submit(function (event) {
+  $('#formChar').submit(function (event) {
     query();
     return false;
   });
-  $("#txtChar").change(function (event) {
-    $("#formChar").submit();
+  $('#txtChar').change(function (event) {
+    $('#formChar').submit();
   });
 
   // Resize txtChar
-  $("#txtChar").focus(function (event) {
-    $("#txtChar").width("7em");
-    $("#selSearch").hide();
+  $('#txtChar').focus(function (event) {
+    $('#txtChar').width('7em');
+    $('#selSearch').hide();
   });
-  $("#txtChar").blur(function (event) {
-    $("#txtChar").width("1.5em");
-    $("#selSearch").show();
+  $('#txtChar').blur(function (event) {
+    $('#txtChar').width('1.5em');
+    $('#selSearch').show();
   });
 
   // Search functions
   var websites = {
-    btnWWWJDIC: "http://www.edrdg.org/cgi-bin/wwwjdic/wwwjdic?1MMJ",
-    btnYamasa: "http://www.yamasa.cc/ocjs/kanjidic.nsf/SortedByKanji2THEnglish/",
-    btnKDamage: "http://kanjidamage.com/kanji/search?q=",
-    btnWikiE: "http://en.wiktionary.org/wiki/",
-    btnWikiJ: "http://ja.wiktionary.org/wiki/",
-    btnKotobank: "https://kotobank.jp/word/",
-    btnCEty: "http://www.chineseetymology.org/CharacterEtymology.aspx?characterInput=",
-    btnCChr: "http://chinese-characters.org/cgi-bin/lookup.cgi?characterInput=",
-    btnChise: "http://www.chise.org/chisewiki/view.cgi?character=",
-    btnNico: "http://dic.nicovideo.jp/a/",
-    btnKago: "http://www5b.biglobe.ne.jp/~shu-sato/kanji/kago.htm",
-    btnKNet: "http://www.kanjinetworks.com/eng/kanji-dictionary/online-kanji-etymology-dictionary.cfm?kanji_id=",
-    btnZDic: "http://www.zdic.net/sousuo?q=",
-    btnGGArt: "http://www.iguci.cn/dictionary/dcontent.php?word=",
-    btnXiu: "http://www.xiuwenyuan.com/ziyuan/",
+    btnWWWJDIC: 'http://www.edrdg.org/cgi-bin/wwwjdic/wwwjdic?1MMJ',
+    btnYamasa: 'http://www.yamasa.cc/ocjs/kanjidic.nsf/SortedByKanji2THEnglish/',
+    btnKDamage: 'http://kanjidamage.com/kanji/search?q=',
+    btnWikiE: 'http://en.wiktionary.org/wiki/',
+    btnWikiJ: 'http://ja.wiktionary.org/wiki/',
+    btnKotobank: 'https://kotobank.jp/word/',
+    btnCEty: 'http://www.chineseetymology.org/CharacterEtymology.aspx?characterInput=',
+    btnCChr: 'http://chinese-characters.org/cgi-bin/lookup.cgi?characterInput=',
+    btnChise: 'http://www.chise.org/chisewiki/view.cgi?character=',
+    btnNico: 'http://dic.nicovideo.jp/a/',
+    btnKago: 'http://www5b.biglobe.ne.jp/~shu-sato/kanji/kago.htm',
+    btnKNet: 'http://www.kanjinetworks.com/eng/kanji-dictionary/online-kanji-etymology-dictionary.cfm?kanji_id=',
+    btnZDic: 'http://www.zdic.net/sousuo?q=',
+    btnGGArt: 'http://www.iguci.cn/dictionary/dcontent.php?word=',
+    btnXiu: 'http://www.xiuwenyuan.com/ziyuan/',
   };
 
   var kagoMap = {}, xiuwenyuanMap;
@@ -309,44 +222,44 @@ $(function () {
   });
 
   var goToSite = function () {
-    if ($("#theChar").text() === BLANK) return;
-    var sitename = $("#selSearch").val();
-    var target = "";
-    if (sitename === "btnKago") {
+    if ($('#theChar').text() === BLANK) return;
+    var sitename = $('#selSearch').val();
+    var target = '';
+    if (sitename === 'btnKago') {
       target = websites[sitename];
-      var anchor = kagoMap[$("#theChar").text()];
+      var anchor = kagoMap[$('#theChar').text()];
       if (anchor !== undefined) {
         target += ('#' + anchor);
       }
-    } else if (sitename === "btnXiu") {
+    } else if (sitename === 'btnXiu') {
       target = websites[sitename];
-      var page = xiuwenyuanMap[$("#theChar").text()];
+      var page = xiuwenyuanMap[$('#theChar').text()];
       if (page !== undefined) {
-        target += (page + ".html");
+        target += (page + '.html');
       }
-    } else if (sitename !== "btnX") {
-      target = websites[sitename] + $("#theChar").text();
+    } else if (sitename !== 'btnX') {
+      target = websites[sitename] + $('#theChar').text();
     }
-    $("#extFrame").attr('src', target);
+    $('#extFrame').attr('src', target);
   };
-  $("#selSearch").change(goToSite);
+  $('#selSearch').change(goToSite);
 
   // Layout functions
   var resizer = function () {
     var sel_height = ($(window).height() - 20
-                      - $("#charSelUpper").outerHeight(true));
-    $("#charSelLower").height(sel_height);
+                      - $('#charSelUpper').outerHeight(true));
+    $('#charSelLower').height(sel_height);
     var desc_height = ($(window).height() - 20
-                       - $("#charDispInner").outerHeight(true));
-    $("#charDesc").height(desc_height);
+                       - $('#charDispInner').outerHeight(true));
+    $('#charDesc').height(desc_height);
   };
   $(window).resize(resizer);
   resizer();
 
   // Reset fields
-  $("#txtChar").val("");
-  $("#selSearch").val("btnX");
-  $("#charDesc").hide();
-  $("#extFrame").attr('src', "");
+  $('#txtChar').val('');
+  $('#selSearch').val('btnX');
+  $('#charDesc').hide();
+  $('#extFrame').attr('src', '');
 
 });
